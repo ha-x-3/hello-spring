d94fb80a4d16346bbec7f20759f29df0ccfde306
Brittney Epperson <beppers87@hotmail.com>
2023-09-20 14:30:03 -0500

    Completed exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -20,10 +20,10 @@ public class HelloController {
     }
 
 //    @GetMapping("hello")
-    @RequestMapping(method={RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
-    }
+//    @RequestMapping(method={RequestMethod.GET, RequestMethod.POST})
+//    public String helloWithQueryParam(@RequestParam String name) {
+//        return "Hello, " + name + "!";
+//    }
 
     @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name) {
@@ -36,10 +36,48 @@ public class HelloController {
                     "<body>" +
                         "<form action='hello' method='post'>" +
                             "<input type='text' name='name'>" +
+                            "<select name='language' id='language-select'>" +
+                                "<option value='english'>English</option>" +
+                                "<option value='spanish'>Spanish</option>" +
+                                "<option value='french'>French</option>" +
+                                "<option value='german'>German</option>" +
+                                "<option value='dothraki'>Dothraki</option>" +
+                            "</select>" +
                             "<input type='submit' value='Greet me!'>" +
                         "</form>" +
                     "</body>" +
                 "</html>";
+    }
+
+    @RequestMapping(value="hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if (name == null || name.isEmpty()) {
+            name = "World";
+        }
+        return createMessage(name, language);
+    }
+
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+        if (l.equals("english")) {
+            greeting = "Hello";
+        }
+        if (l.equals("spanish")) {
+            greeting = "Hola";
+        }
+        if (l.equals("french")) {
+            greeting = "Bonjour";
+        }
+        if (l.equals("german")) {
+            greeting = "Hallo";
+        }
+        if (l.equals("dothraki")) {
+            greeting = "Athchomar chomakaan";
+        }
+
+        return "<h1 style='color: purple; font-family: Helvetica; text-align: center; padding-top: 50px;'>"
+                + greeting + ", " + n + "!" + "</h>";
     }
 
 }
